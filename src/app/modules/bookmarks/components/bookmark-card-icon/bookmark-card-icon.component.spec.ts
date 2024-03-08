c3f7ee68b29f03e235352c843666757de695c0cd
Vladyslav Tereshchenko <tereshchenkovladyslav4@outlook.com>
2024-03-08 16:03:36 -0500

    bookmark-card-icon unit test

--- src/app/modules/bookmarks/components/bookmark-card-icon/bookmark-card-icon.component.spec.ts
+++ src/app/modules/bookmarks/components/bookmark-card-icon/bookmark-card-icon.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { BookmarkCardIconComponent } from './bookmark-card-icon.component';
+import { BookmarksStateService } from '../../../../state/bookmarks/bookmarks-state.service';
+import { of } from 'rxjs';
 
 describe('BookmarkCardIconComponent', () => {
   let component: BookmarkCardIconComponent;
@@ -8,9 +10,8 @@ describe('BookmarkCardIconComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ BookmarkCardIconComponent ]
-    })
-    .compileComponents();
+      declarations: [],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(BookmarkCardIconComponent);
     component = fixture.componentInstance;
@@ -19,5 +20,19 @@ describe('BookmarkCardIconComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should render the bookmark-wrap div with white class', () => {
+    component.isWhite = true;
+    fixture.detectChanges();
+
+    expect(fixture.nativeElement.querySelector('.bookmark-wrap').classList.contains('white')).toBe(true);
+  });
+
+  it('should render the bookmark-wrap div without white class', () => {
+    component.isWhite = false;
+    fixture.detectChanges();
+
+    expect(fixture.nativeElement.querySelector('.bookmark-wrap').classList.contains('white')).toBe(false);
   });
 });
